893188e58515ac0484a11b7e617abcff20b465ac
AgboVincent <agbovincent100@gmail.com>
2022-07-03 23:08:47 +0100

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+        title: "A pillow",
+        imageUrl: "",
+        price: 19.99,
+        description: "A soft pillow"
+    },
+    {
+        title: "A Carpet",
+        imageUrl: "",
+        price: 89.99,
+        description: "A carpet which you might like or not"
+    }
+];
+
+
+const productsList = {
+    
+}
